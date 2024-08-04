659f1d1b2f3b0e0f199d24cd2a85a0cbf29e555b
11wtaiga <r202204514om@jindai.jp>
2024-08-04 13:45:38 +0900

    ペジネーション

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,6 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('Posts.index')->with(['posts' => $post->get()]);
+        return view('Posts.index')->with(['posts' => $post->getPaginateByLimit(1)]);
     }
 }
