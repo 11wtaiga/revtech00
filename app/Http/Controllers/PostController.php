a7b0d1484e30679d28c8942f4b4f6edd23df2175
11wtaiga <r202204514om@jindai.jp>
2024-08-04 15:05:11 +0900

    ブログ詳細表示

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\post;
+use App\Models\Post;
 
 class PostController extends Controller
 {
+    public function show(Post $post)
+    {
+        return view('posts.show')->with(['post' => $post]);
+        //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+    }
+    
     public function index(Post $post)
     {
-        return $post->get();
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(1)]);
     }
 }
